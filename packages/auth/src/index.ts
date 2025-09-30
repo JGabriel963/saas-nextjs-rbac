bdc5923d6d4a53df160877dd52b600ea3cf410d4
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-09-30 14:22:53 -0300

    refact: Set type with zod

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,30 @@
+import {
+  createMongoAbility,
+  ForcedSubject,
+  CreateAbility,
+  MongoAbility,
+  AbilityBuilder,
+} from "@casl/ability";
+import { User } from "./models/user";
+import { permissions } from "./permissions";
+import { UserSubject } from "./subjects/user";
+import { ProjectSubject } from "./subjects/project";
+
+type AppAbilities = UserSubject | ProjectSubject | ["manage", "all"];
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility);
+
+  if (typeof permissions[user.role] !== "function") {
+    throw new Error(`Permissions for role ${user.role} no found.`);
+  }
+
+  permissions[user.role](user, builder);
+
+  const ability = builder.build();
+
+  return ability;
+}
