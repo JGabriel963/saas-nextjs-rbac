e79b64a7992d4bf4cde6d3fb0fea71a9503c5f0c
João Gabriel <joaogabriel9633@gmail.com>
2025-10-11 22:26:13 -0300

    initalized front-end

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Saas RBCA Next",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body>{children}</body>
+    </html>
+  );
+}
